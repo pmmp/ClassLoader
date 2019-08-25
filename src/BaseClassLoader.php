c2440109d1f37c8bc47735345dbc749f36d5d7db
Dylan K. Taylor <odigiman@gmail.com>
2019-08-25 19:20:31 +0100

    remove useless crap from BaseClassLoader
    i'd like to have this thing completely immutable, but that's going to take more work.

--- src/BaseClassLoader.php
+++ src/BaseClassLoader.php
@@ -17,21 +17,11 @@
 
 class BaseClassLoader extends \Threaded implements DynamicClassLoader{
 
-	/** @var \ClassLoader */
-	private $parent;
 	/** @var \Threaded|string[] */
 	private $lookup;
-	/** @var \Threaded|string[] */
-	private $classes;
 
-
-	/**
-	 * @param ClassLoader $parent
-	 */
-	public function __construct(ClassLoader $parent = null){
-		$this->parent = $parent;
+	public function __construct(){
 		$this->lookup = new \Threaded;
-		$this->classes = new \Threaded;
 	}
 
 	/**
@@ -76,43 +66,6 @@ class BaseClassLoader extends \Threaded implements DynamicClassLoader{
 	}
 
 	/**
-	 * Removes a path from the lookup list
-	 *
-	 * @param string $path
-	 *
-	 * @return void
-	 */
-	public function removePath($path){
-		foreach($this->lookup as $i => $p){
-			if($p === $path){
-				unset($this->lookup[$i]);
-			}
-		}
-	}
-
-	/**
-	 * Returns an array of the classes loaded
-	 *
-	 * @return string[]
-	 */
-	public function getClasses(){
-		$classes = [];
-		foreach($this->classes as $class){
-			$classes[] = $class;
-		}
-		return $classes;
-	}
-
-	/**
-	 * Returns the parent ClassLoader, if any
-	 *
-	 * @return ClassLoader
-	 */
-	public function getParent(){
-		return $this->parent;
-	}
-
-	/**
 	 * Attaches the ClassLoader to the PHP runtime
 	 *
 	 * @param bool $prepend
@@ -141,8 +94,6 @@ class BaseClassLoader extends \Threaded implements DynamicClassLoader{
 			if(method_exists($name, "onClassLoaded") and (new ReflectionClass($name))->getMethod("onClassLoaded")->isStatic()){
 				$name::onClassLoaded();
 			}
-			
-			$this->classes[] = $name;
 
 			return true;
 		}
