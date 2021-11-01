8027a61395c08cee128ebd8e8fe30b655b9f1475
Dylan K. Taylor <dktapps@pmmp.io>
2021-11-01 19:17:10 +0000

    Revert "BaseClassLoader no longer extends Threaded"
    This reverts commit 80226e0917be79ac3230606113e25134a31e6a85.
    
    this caused compatibility issues with DEVirion and doesn't really offer
    any significant performance advantage anyway.

--- src/BaseClassLoader.php
+++ src/BaseClassLoader.php
@@ -15,15 +15,7 @@
  * GNU General Public License for more details.
 */
 
-class BaseClassLoader implements DynamicClassLoader{
-
-	/*
-	 * Note for future maintainers: This class doesn't need to extend `Threaded` because of pthreads' serialization
-	 * trickery - the lookup Threaded objects will be shared between threads after deserialization, even though the
-	 * actual BaseClassLoader objects will be distinct (not shared).
-	 * It's done this way to bypass useless extra locking - since the BaseClassLoader itself doesn't have any
-	 * non-Threaded fields, it doesn't make any difference whether it's Threaded or not, except for performance.
-	 */
+class BaseClassLoader extends \Threaded implements DynamicClassLoader{
 
 	/** @var \Threaded|string[] */
 	private $fallbackLookup;
